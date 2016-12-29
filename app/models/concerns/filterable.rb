13961b4fb0e4714a5df62bb11d5e245c29da1d9e
Mateus <mateussabino87@gmail.com>
2016-12-29 21:25:55 -0200

    refactor: filterable

--- app/models/concerns/filterable.rb
+++ app/models/concerns/filterable.rb
@@ -4,7 +4,7 @@ module Filterable
   included do
     self::FILTER_METHODS.each do |method_name|
       self.define_singleton_method("by_#{method_name}".to_sym) do |arg|
-        where("#{method_name}": arg)
+        where(method_name => arg)
       end
     end
 
