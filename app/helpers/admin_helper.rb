1a6180425ad1047b905c6563b2e36f23c87999d6
Mateus <mateussabino87@gmail.com>
2017-01-08 17:57:01 -0200

    refactor: admin index

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,3 +1,11 @@
 module AdminHelper
 
+  def render_header_description(description)
+    render 'admin/shared/header_description', description_text: description
+  end
+
+  def render_breadcrumb
+    render 'admin/shared/breadcrumb'
+  end
+
 end
