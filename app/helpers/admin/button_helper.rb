4be0892fc6b34dea4f48af2846cd3180c553f595
Mateus <mateussabino87@gmail.com>
2017-01-08 15:49:32 -0200

    refactor form layouts

--- app/helpers/admin/button_helper.rb
+++ app/helpers/admin/button_helper.rb
@@ -4,4 +4,8 @@ module Admin::ButtonHelper
     link_to button_text, path, class: 'btn btn-primary btn-block', role: "button"
   end
 
+  def save_button(form)
+    form.button :submit, class: 'btn btn-primary pull-right'
+  end
+
 end
